a9823fe49185b9e8f865e6522275ae6ad86b6e6c
mulder21c <mulder21c@gmail.com>
2022-12-07 12:58:44 +0900

    feat(quiz): made quizzes

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -1,13 +1,16 @@
+const mode = process.env.BABEL_ENV;
+const root = mode === "production" ? "/a11y/" : "/";
+
 const config = {
-  title: "Teach Access Tutorial",
-  tagline: "Best practices for making accessible mobile and web apps",
-  url: "https://teachaccess.github.io",
-  baseUrl: "/",
-  organizationName: "teachaccess",
-  projectName: "tutorial",
+  title: "사람인 웹 접근성 교육",
+  tagline: "",
+  url: "https://saramin.github.io/",
+  baseUrl: root,
+  organizationName: "saramin",
+  projectName: "a11y",
   deploymentBranch: "gh-pages",
-  scripts: ["https://buttons.github.io/buttons.js", "/js/scriptHelper.js"],
-  favicon: "../img/logo-teach-access.svg",
+  scripts: ["https://buttons.github.io/buttons.js"],
+  favicon: `${root}img/saramin.png`,
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "throw",
   trailingSlash: false,
@@ -16,8 +19,8 @@ const config = {
       "@docusaurus/preset-classic",
       {
         docs: {
-          showLastUpdateAuthor: true,
-          showLastUpdateTime: true,
+          showLastUpdateAuthor: false,
+          showLastUpdateTime: false,
           path: "../docs",
           sidebarPath: require.resolve("./sidebars.js"),
           routeBasePath: "/",
@@ -32,33 +35,39 @@ const config = {
   themeConfig: {
     navbar: {
       logo: {
-        src: "../img/logo-teach-access.svg",
-        srcDark: "../img/logo-teach-access-dark.svg",
-        alt: "Teach Access",
+        src: `${root}img/logo-saramin.svg`,
+        srcDark: `${root}img/logo-saramin-dark.svg`,
+        alt: "사람인",
       },
       items: [
         {
-          to: "/",
-          label: "Tutorial",
+          to: `${root}quiz/1`,
+          label: "퀴즈",
           position: "left",
         },
         {
-          to: "/help",
-          label: "Help",
+          to: "https://sri-fe1.notion.site/Web-Accessibility-101-f9fbf1776dcc4641a5858cdd1d5432c5",
+          label: "웹 접근성 101",
+          position: "left",
+        },
+        {
+          to: "https://sri-fe1.notion.site/7f63c1d4dff04051b4fff1390628f99c",
+          label: "사람인 웹 접근성 가이드",
           position: "left",
         },
       ],
     },
-    image: "../img/undraw_online.svg",
+    image: `${root}img/undraw_online.svg`,
     footer: {
       links: [],
-      copyright: "Copyright © 2021 Teach Access",
+      copyright: "Copyright © 2022 사람인",
       logo: {
-        src: "../img/logo-teach-access.svg",
-        srcDark: "../img/logo-teach-access-dark.svg",
-        alt: "Teach Access",
+        src: `${root}img/logo-saramin.svg`,
+        srcDark: `${root}img/logo-saramin-dark.svg`,
+        alt: "사람인",
       },
     },
+    tableOfContents: {},
   },
 };
 
--- website/sidebars.js
+++ website/sidebars.js
@@ -1,28 +1,39 @@
 module.exports = {
   docs: {
-    "1. Introduction": ["introduction/using-this-tutorial", "introduction/faq"],
-    "2. Writing Code": [
-      "code/introduction",
-      "code/headings",
-      "code/images",
-      "code/keyboard-navigation",
-      "code/labels",
-      "code/lists",
-      "code/dialogs",
-      "code/tables",
-      "code/menus",
-      "code/aria-introduction",
-      "code/aria",
-      "code/checklist",
-    ],
-    "3. Design Principles": [
-      "design/introduction",
-      "design/color-contrast",
-      "design/color-meaning",
-      "design/text-size",
-      "design/site-copy",
-      "design/photos",
-      "design/checklist",
+    "웹 접근성 퀴즈": [
+      {
+        type: "category",
+        label: "접근성 일반",
+        collapsed: false,
+        items: [
+          "quiz/quiz-1",
+          "quiz/quiz-2",
+          "quiz/quiz-3",
+          "quiz/quiz-4",
+          "quiz/quiz-5",
+        ],
+      },
+      {
+        type: "category",
+        label: "장애 이해",
+        collapsed: false,
+        items: ["quiz/quiz-6", "quiz/quiz-7"],
+      },
+      {
+        type: "category",
+        label: "접근성 사례",
+        collapsed: false,
+        items: [
+          "quiz/quiz-8",
+          "quiz/quiz-9",
+          "quiz/quiz-10",
+          "quiz/quiz-11",
+          "quiz/quiz-12",
+          "quiz/quiz-13",
+          "quiz/quiz-14",
+          "quiz/quiz-15",
+        ],
+      },
     ],
   },
 };
